348bb076ab8ab68731dd52fe0a72c6b67f6b3778
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-05-01 16:13:26 +0200

    ABI_V0 compatibility: Poseidon, use AROS_SOFTINT instead of AROS_INT for Cause and AddResetCallback

--- rom/usb/pciusb/ehcichip.c
+++ rom/usb/pciusb/ehcichip.c
@@ -18,7 +18,8 @@
 #undef HiddAttrBase
 #define HiddAttrBase (hd->hd_HiddAB)
 
-static AROS_INTH1(EhciResetHandler, struct PCIController *, hc)
+/* ABI_V0 compatibility */
+static AROS_SOFTINTH1(EhciResetHandler, struct PCIController *, hc)
 {
     AROS_INTFUNC_INIT
 
@@ -1090,7 +1091,8 @@ void ehciUpdateFrameCounter(struct PCIController *hc) {
     Enable();
 }
 
-static AROS_INTH1(ehciCompleteInt, struct PCIController *, hc)
+/* ABI_V0 compatibility */
+static AROS_SOFTINTH1(ehciCompleteInt, struct PCIController *, hc)
 {
     AROS_INTFUNC_INIT
 
