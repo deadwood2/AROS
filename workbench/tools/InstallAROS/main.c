82261c77b9c689c289e6f1d60b59a5f47551324c
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-10-15 17:06:08 +0200

    Other: InstallAROS - revert to having ata.device as default selection option

--- workbench/tools/InstallAROS/main.c
+++ workbench/tools/InstallAROS/main.c
@@ -149,7 +149,7 @@ char *dest_Path = NULL;         /* DOS DEVICE NAME of part used to store "aros" 
 char *work_Path = NULL;         /* DOS DEVICE NAME of part used to store "work" */
 TEXT *extras_path = NULL;       /* DOS DEVICE NAME of part used to store extras */
 
-char *boot_Device = "ahci.device";
+char *boot_Device = "ata.device";
 ULONG boot_Unit = 0;
 
 Object *check_copytowork = NULL;
@@ -3204,8 +3204,8 @@ int main(int argc, char *argv[])
         MUIA_Gauge_Current, 0, End);
 
     static char *opt_drivetypes[] = {
-        "AHCI/SATA",
         "IDE",
+        "AHCI/SATA",
         "USB",
         NULL
     };
@@ -3770,10 +3770,10 @@ int main(int argc, char *argv[])
     /* Notifications upon selection of drive type */
     DoMethod(cycle_drivetype, MUIM_Notify, (IPTR) MUIA_Cycle_Active, 0,
         (IPTR) dest_device, 3, MUIM_Set,
-        MUIA_String_Contents, "ahci.device");
+        MUIA_String_Contents, "ata.device");
     DoMethod(cycle_drivetype, MUIM_Notify, (IPTR) MUIA_Cycle_Active, 1,
         (IPTR) dest_device, 3, MUIM_Set,
-        MUIA_String_Contents, "ata.device");
+        MUIA_String_Contents, "ahci.device");
     DoMethod(cycle_drivetype, MUIM_Notify, (IPTR) MUIA_Cycle_Active, 2,
         (IPTR) dest_device, 3, MUIM_Set,
         MUIA_String_Contents, "usbscsi.device");
