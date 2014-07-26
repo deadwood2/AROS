645979ea48492ad954081a81430347c59bc465bc
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2014-07-26 16:14:41 +0200

    ABI_V0 compatibility: arosc.library - re-introduce spawnv / spawnvp

---
+++ compiler/clib/__spawnv.h
@@ -0,0 +1,15 @@
+#ifndef ___SPAWN_H
+#define ___SPAWN_H
+
+/*
+    Copyright © 1995-2008, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: stat() internals - header file
+    Lang: english
+*/
+
+/* ABI_V0 compatibility */
+int __spawnv(int mode, const char *filename, int searchpath, char *const argv[]);
+
+#endif
