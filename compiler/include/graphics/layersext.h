cec6c9673119af008e51a54c9b0f4d852961e11f
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2014-07-26 18:14:09 +0200

    ABI_V0 compatibility: graphics.library - revert to compatible definitions for LA_ tags where possible

--- compiler/include/graphics/layersext.h
+++ compiler/include/graphics/layersext.h
@@ -16,9 +16,9 @@
 /* Tags for CreateLayerTagList */
 
 /* AmigaOS4-compatible */
-#define LA_ShapeRegion  (TAG_USER + 34)
+#define LA_ShapeRegion  (TAG_USER + 99 + 105)   /* ABI_V0 compatibility */
 #define LA_ShapeHook    (TAG_USER + 35) /* struct Region *. Default is NULL (rectangular shape) */
-#define LA_InFrontOf    (TAG_USER + 36)
+#define LA_InFrontOf    (TAG_USER + 99 + 102)   /* ABI_V0 compatibility */
 #define LA_Hidden	(TAG_USER + 41)	/* BOOL. Default is FALSE */
 
 /* MorphOS-compatible */
@@ -27,11 +27,11 @@
 #define LA_TransRegion  (LA_Dummy + 2) /* struct Region *. Default is NULL (rectangular shape) */
 #define LA_TransHook    (LA_Dummy + 3)
 #define LA_WindowPtr    (LA_Dummy + 4)
-#define LA_SuperBitMap  (LA_Dummy + 5) /* struct BitMap *. Default is NULL (none) */
+#define LA_SuperBitMap  (TAG_USER + 99 + 14) /* struct BitMap *. Default is NULL (none) */ /* ABI_V0 compatibility */
 
 /* AROS-specific */
 #define LA_AROS		(TAG_USER + 1234)
-#define LA_Behind	(LA_AROS + 3)
-#define LA_ChildOf	(LA_AROS + 4)
+#define LA_Behind	(TAG_USER + 99 + 103)       /* ABI_V0 compatibility */
+#define LA_ChildOf	(TAG_USER + 99 + 101)       /* ABI_V0 compatibility */
 
 #endif /* GRAPHICS_LAYERSEXT_H */
