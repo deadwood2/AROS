ce5eda9a3964a6fab82e9841da52884cac3bb204
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2014-07-26 16:14:41 +0200

    ABI_V0 compatibility: arosc.library - re-introduce spawnv / spawnvp

---
+++ compiler/clib/include/process.h
@@ -0,0 +1,23 @@
+#ifndef _PROCESS_H_
+#define _PROCESS_H_
+
+/*
+    Copyright © 2003-2011, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <aros/system.h>
+
+__BEGIN_DECLS
+
+/* ABI_V0 compatibility */
+
+#define P_WAIT      0  /* Wait for the child process to terminate.  */
+#define P_NOWAIT    1  /* Execute the parent and the child concurrently.  */
+#define P_OVERLAY   2  /* Replace parent's image with child's. Same as exec*(3).  */
+
+int spawnv(int mode, const char *path, char *const argv[]);
+int spawnvp(int mode, const char *path, char *const argv[]);
+
+__END_DECLS
+#endif /* _PROCESS_H_ */
