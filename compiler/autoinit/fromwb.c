1236f9f5c06be5aabdfd95257610e3859b6bb3c0
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2014-08-23 21:28:48 +0200

    Forward compatibility: autoinit.library - fetch the message posted by Workbench as early as possible
    This message is posted to DOS message port and can be considered a
    reply to a packet send to DOS - which breaks DOS functions.

--- compiler/autoinit/fromwb.c
+++ compiler/autoinit/fromwb.c
@@ -6,6 +6,7 @@
 */
 #include <aros/symbolsets.h>
 #include <aros/startup.h>
+#include <dos/dosextens.h>
 #include <proto/autoinit.h>
 
 struct WBStartup *WBenchMsg;
@@ -16,7 +17,7 @@ static void __startup_fromwb(struct ExecBase *SysBase)
 {
     BPTR curdir = BNULL;
 
-    FromWBInit(&WBenchMsg, &curdir, &__argv, &__argc);
+    FromWBInit2(WBenchMsg, &curdir, (STRPTR **)&__argv, &__argc);
 
     __startup_entries_next();
 
@@ -24,3 +25,27 @@ static void __startup_fromwb(struct ExecBase *SysBase)
 }
 
 ADD2SET(__startup_fromwb, PROGRAM_ENTRIES, -50);
+
+/*
+ * This should be executed as one of the first actions. The message
+ * that is present on myproc->pr_MsgPort will break DOS functions
+ * if not fetched. (example: autopened library using DOS in its
+ * OpenLib function).
+ */
+static void __startup_fromwb_prelibopen(struct ExecBase *SysBase)
+{
+    struct Process *myproc = (struct Process *)FindTask(NULL);
+
+     /* Do we have a CLI structure? */
+     if (!myproc->pr_CLI)
+     {
+         /* Workbench startup. Get WBenchMsg and pass it to main() */
+
+         WaitPort(&myproc->pr_MsgPort);
+         WBenchMsg = (struct WBStartup *)GetMsg(&myproc->pr_MsgPort);
+     }
+
+    __startup_entries_next();
+}
+
+ADD2SET(__startup_fromwb_prelibopen, PROGRAM_ENTRIES, -70);
--- compiler/autoinit/fromwbinit.c
+++ compiler/autoinit/fromwbinit.c
@@ -86,3 +86,67 @@
 
     AROS_LIBFUNC_EXIT
 } /* FromWBInit */
+
+#undef WBenchMsg
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/autoinit.h>
+
+        AROS_LH4(BOOL, FromWBInit2,
+
+/*  SYNOPSIS */
+        AROS_LHA(struct WBStartup *, WBenchMsg, A1),
+        AROS_LHA(BPTR *, curdirp, A2),
+        AROS_LHA(STRPTR **, __argvp, A4),
+        AROS_LHA(LONG *, __argcp, A5),
+
+
+/*  LOCATION */
+        struct Library *, AutoinitBase, 12, Autoinit)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+        If the startup code has to change in backwards incompatible way
+        do not change this function. Create a new one and keep this compatible
+        so that already compiled executatables continue running.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    D(bug("Entering __startup_fromwb()\n"));
+
+    if (WBenchMsg)
+    {
+        __argv = (STRPTR *) WBenchMsg;
+        __argc = 0;
+
+        /* ABI_V0 compatibility */
+#if 0
+        /* WB started processes' pr_CurrentDir = BNULL */
+        curdir = DupLock(WBenchMsg->sm_ArgList->wa_Lock);
+        CurrentDir(curdir);
+#endif
+
+        D(bug("[startup] Started from Workbench\n"));
+    }
+
+    return TRUE;
+
+    AROS_LIBFUNC_EXIT
+} /* FromWBInit2 */
