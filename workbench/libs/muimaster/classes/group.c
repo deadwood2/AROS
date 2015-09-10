6665a496a09446e06daeba9a63da400e75cd7ea3
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2015-09-10 14:49:06 +0200

    ABI_V0 compatibility: muimaster.library - restore Group.mui refresh behaviour

--- workbench/libs/muimaster/classes/group.c
+++ workbench/libs/muimaster/classes/group.c
@@ -885,6 +885,8 @@ IPTR Group__MUIM_ExitChange(struct IClass *cl, Object *obj,
         if ((_flags(obj) & MADF_SETUP) && _win(obj))
         {
             Object *win = _win(obj);
+#if 0
+            /* ABI_V0 compatibility */
             Object *parent = obj;
 
             /* CHECKME: Don't call RecalcDisplay if one of our parents is
@@ -907,6 +909,7 @@ IPTR Group__MUIM_ExitChange(struct IClass *cl, Object *obj,
                 }
 
             }
+#endif
 
             DoMethod(win, MUIM_Window_RecalcDisplay, (IPTR) obj);
         }
@@ -931,6 +934,8 @@ IPTR Group__MUIM_ExitChange2(struct IClass *cl, Object *obj,
         if ((_flags(obj) & MADF_SETUP) && _win(obj))
         {
             Object *win = _win(obj);
+#if 0
+            /* ABI_V0 compatibility */
             Object *parent = obj;
 
             /* CHECKME: Don't call RecalcDisplay if one of our parents is
@@ -953,6 +958,7 @@ IPTR Group__MUIM_ExitChange2(struct IClass *cl, Object *obj,
                 }
 
             }
+#endif
 
             DoMethod(win, MUIM_Window_RecalcDisplay, (IPTR) obj);
         }
