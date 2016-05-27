5090704d7b63e4e0a7a939688e2e540dd715a899
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2016-05-27 11:30:11 +0200

    AROSC: reintroduce arosc.library
    Headers shared with stdc and posixc.
    
    libm.a has been merged into linklib for arosc.library (libarosc.a)
    
    Arch-dependant files are only restored for i386 and arm.

---
+++ compiler/clib/__ctype_linklib.c
@@ -0,0 +1,34 @@
+/*
+    Copyright © 2012, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <sys/arosc.h>
+#include <aros/symbolsets.h>
+
+#include <proto/exec.h>
+
+#include "__arosc_privdata.h"
+
+const unsigned short int * const *__ctype_b_ptr = NULL;
+const unsigned char * const *__ctype_toupper_ptr = NULL;
+const unsigned char * const *__ctype_tolower_ptr = NULL;
+
+static int __ctype_init(struct ExecBase *SysBase)
+{
+    const struct arosc_ctype *ctype;
+    struct aroscbase *aroscbase = __aros_getbase_aroscbase();
+
+    if (!aroscbase)
+    	    return 0;
+
+    ctype = &aroscbase->acb_acud.acud_ctype;
+
+    __ctype_b_ptr = &ctype->b;
+    __ctype_toupper_ptr = &ctype->toupper;
+    __ctype_tolower_ptr = &ctype->tolower;
+
+    return 1;
+}
+
+ADD2INIT(__ctype_init, 20);
