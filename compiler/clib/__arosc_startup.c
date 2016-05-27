950ed0e4169c9e8d54295c169b830736558b1662
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2016-05-27 11:52:11 +0200

    Other: arosc.library - set the stderr buffering mode during creation of stream

--- compiler/clib/__arosc_startup.c
+++ compiler/clib/__arosc_startup.c
@@ -75,21 +75,11 @@ static void abiv0_delayed_startup()
 ******************************************************************************/
 {
     struct aroscbase *aroscbase = __aros_getbase_aroscbase();
-    struct Process *me = (struct Process *)FindTask(NULL);
 
-    D(bug("[__arosc_program_startup] aroscbase 0x%p\n", aroscbase));
+    D(bug("[__arosc_program_startup_abiv0] aroscbase 0x%p\n", aroscbase));
 
     aroscbase->acb_startup_error_ptr = NULL;
     aroscbase->acb_flags |= ACPD_NEWSTARTUP;
-
-    /* A some C error IO routines evidently rely on this, and
-     * should be fixed!
-     */
-    if (me->pr_Task.tc_Node.ln_Type == NT_PROCESS &&
-        me->pr_CES != BNULL)
-    {
-        SetVBuf(me->pr_CES, NULL, BUF_NONE, -1);
-    }
 }
 
 /*****************************************************************************
@@ -128,22 +118,12 @@ static void abiv0_delayed_startup()
 ******************************************************************************/
 {
     struct aroscbase *aroscbase = __aros_getbase_aroscbase();
-    struct Process *me = (struct Process *)FindTask(NULL);
 
     D(bug("[__arosc_program_startup] aroscbase 0x%p\n", aroscbase));
 
     aroscbase->acb_startup_error_ptr = errorptr;
     *aroscbase->acb_exit_jmp_buf = *exitjmp;
     aroscbase->acb_flags |= ACPD_NEWSTARTUP;
-
-    /* A some C error IO routines evidently rely on this, and
-     * should be fixed!
-     */
-    if (me->pr_Task.tc_Node.ln_Type == NT_PROCESS &&
-        me->pr_CES != BNULL)
-    {
-        SetVBuf(me->pr_CES, NULL, BUF_NONE, -1);
-    }
 }
 
 /*****************************************************************************
