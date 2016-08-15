3ae46f1274c2b75176edb62210ff57f7113dd5ed
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2016-08-15 13:04:49 +0200

    Other: arosc.library - restore original uid functions and add define for tcsetattr

---
+++ compiler/clib/setuid.c
@@ -0,0 +1,44 @@
+/*
+    Copyright © 2004-2013, The AROS Development Team. All rights reserved.
+    $Id$
+
+    POSIX.1-2008 function setuid().
+*/
+
+#include <aros/debug.h>
+#include <errno.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <unistd.h>
+
+	int setuid(
+
+/*  SYNOPSIS */
+	uid_t uid)
+
+/*  FUNCTION
+	
+    INPUTS
+	
+    RESULT
+	
+    NOTES
+        Not implemented.
+
+    EXAMPLE
+
+    BUGS
+    	
+    SEE ALSO
+        
+    INTERNALS
+
+******************************************************************************/
+{
+    /* TODO: Implement setuid() */
+    errno = ENOSYS;
+
+    return 0;
+} /* setuid() */
