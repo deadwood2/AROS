c1109f2bf394477b556c30e9232cbab055888086
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2016-08-15 13:04:49 +0200

    Other: arosc.library - restore original uid functions and add define for tcsetattr

---
+++ compiler/clib/geteuid.c
@@ -0,0 +1,39 @@
+/*
+    Copyright © 2003-2013, The AROS Development Team. All rights reserved.
+    $Id$
+
+    POSIX.1-2008 function geteuid().
+*/
+
+/*****************************************************************************
+
+    NAME */
+#include <unistd.h>
+
+	uid_t geteuid(
+
+/*  SYNOPSIS */
+	void)
+
+/*  FUNCTION
+	
+    INPUTS
+	
+    RESULT
+	
+    NOTES
+        Always returns 0 for the moment.
+
+    EXAMPLE
+
+    BUGS
+    	
+    SEE ALSO
+    	getuid()
+        
+    INTERNALS
+
+******************************************************************************/
+{
+    return 0;
+} /* geteuid() */
