06538a17900b3e6c45d053128957fba8668c2c15
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2016-10-16 20:09:03 +0200

    ABI_V0 compatibility: exec.library and libcall

--- rom/exec/prepareexecbase.c
+++ rom/exec/prepareexecbase.c
@@ -8,6 +8,7 @@
 
 #define DEBUG 0
 
+#include <aros/config.h>
 #include <aros/asmcall.h>
 #include <aros/debug.h>
 #include <aros/kernel.h>
@@ -371,6 +372,11 @@ struct ExecBase *PrepareExecBase(struct MemHeader *mh, struct TagItem *msg)
 #endif
 
     D(bug("[Exec] %s: Preperation complete.\n"));
+
+#if (AROS_FLAVOUR & AROS_FLAVOUR_STANDALONE)
+    /* ABI_V0 compatibility */
+    *(struct ExecBase**)4UL = SysBase;
+#endif
 
     return SysBase;
 }
