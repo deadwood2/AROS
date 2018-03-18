ca44722d0a48659d16a3cc420fbdd4a3dcd90269
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2018-03-18 19:33:13 +0100

    [NP] Change the way symbols sets are handled
    Sections are no longer decorated with priority. The priority is
    now the attribute of marker node and the sorting is done during
    execution of set handler code.
    
    This removes the need for custom sorting in collect-aros and allows
    sets to work with stock linker.

--- compiler/libinit/libinit.c
+++ compiler/libinit/libinit.c
@@ -21,6 +21,30 @@ typedef int (*closedevfunc)
     void *ioreq
 );
 
+static void sort
+(
+    struct Node funcs[],
+    int size,
+    int order
+)
+{
+    int pos;
+    int again = 1;
+
+    if (size == 1) return;
+
+    while (again)
+    {
+        again = 0;
+        for (pos = 0; pos < size - 1; pos++)
+        {
+            if (((funcs[pos].ln_Pri > funcs[pos + 1].ln_Pri) && order > 0) ||
+                ((funcs[pos].ln_Pri < funcs[pos + 1].ln_Pri) && order < 0))
+                { struct Node t = funcs[pos]; funcs[pos] = funcs[pos + 1]; funcs[pos + 1] = t; again = 1; break; }
+        }
+    }
+}
+
 int set_call_libfuncs
 (
     const void * const set[],
@@ -30,12 +54,24 @@ int set_call_libfuncs
 )
 {
     int pos, (*func)(APTR);
+    struct Node *n;
 
     if (!set)
             return TRUE;
 
-    ForeachElementInSet(set, order, pos, func)
+    int size = (IPTR)set[0];
+    struct Node funcs[size];
+
+    ForeachElementInSet(set, 1, pos, n)
     {
+        funcs[pos - 1] = *n;
+    }
+
+    sort(funcs, size, order);
+
+    for(pos = 0; pos < size; pos++)
+    {
+        func = (int (*)(APTR))(funcs[pos].ln_Name);
         if (test_fail)
         {
             if (!(*func)(libbase))
