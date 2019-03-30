07dc6ac207a9a20874f4d318e43d282e5a7cedf9
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2019-03-30 20:36:10 +0100

    [NP] Cumulative Wanderer changes

--- workbench/system/Wanderer/wanderer.c
+++ workbench/system/Wanderer/wanderer.c
@@ -675,6 +675,7 @@ D(bug("[Wanderer] %s: ICONWINDOW_ACTION_OPEN: offset = %d, buf = %s\n", __func__
 
         dropevent->drop_Mode = (ULONG)XGET(prefs, MUIA_IconWindow_IconDropMode);
         {
+asm("int3");
             wandererCopyProcess = CreateNewProcTags(
                                 NP_Entry,       (IPTR)Wanderer__Func_CopyDropEntries,
                                 NP_Name,        (IPTR)wand_copyprocnamestr,
@@ -3512,7 +3513,7 @@ D(bug("[Wanderer] %s() ##\n[Wanderer] %s: Creating 'Workbench' Window..\n", __fu
     {
 D(bug("[Wanderer] %s: Workbench Window Obj @ %x\n", __func__, data->wd_WorkbenchWindow));
 
-        Detach();
+//        Detach();
 
 D(bug("[Wanderer] %s: Really handing control to Zune ..\n", __func__));
 
@@ -3940,7 +3941,7 @@ D(bug("[Wanderer]: %s()\n", __func__));
 
     if ((isWorkbenchWindow = (message->drawer == NULL ? TRUE : FALSE)))
     {
-        useBackdrop = data->wd_Option_BackDropMode;
+//        useBackdrop = data->wd_Option_BackDropMode; //temporarily disable to get window name
     }
 
     TAG_IconWindow_Drawer = isWorkbenchWindow ? TAG_IGNORE : MUIA_IconWindow_Location;
