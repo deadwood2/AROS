31768a219727ca560aa89cbc31f61d33c0ed9b56
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2019-05-03 21:09:48 +0200

    Introduce minimal proxy stdc.library
    At this point no proxying is being done. Linklib is created
    to fulfill build dependencies.

---
+++ arch/all-runtime/clibs/stdc/stdc_intern.h
@@ -0,0 +1,18 @@
+/*
+    Copyright © 2019, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#ifndef STDC_INTERN_H
+#define STDC_INTERN_H
+
+#ifndef EXEC_LIBRARIES_H
+#   include <exec/libraries.h>
+#endif
+
+struct StdCBase
+{
+    struct Library      stdc_Lib;
+};
+
+#endif
