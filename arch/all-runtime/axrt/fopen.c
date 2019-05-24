719719f4140e2b231a4f425b4be3d48c17f9adf6
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2019-05-24 20:20:28 +0200

    First implementation of fopen proxy with Amiga path resolution.

---
+++ arch/all-runtime/axrt/fopen.c
@@ -0,0 +1,119 @@
+/*
+    Copyright © 2019, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <dlfcn.h>
+#include <string.h>
+
+#include <proto/exec.h>
+#include <proto/dos.h>
+
+struct aa
+{
+    struct ExecBase *sysBase;
+    struct Library *dosBase;
+};
+
+struct aa AA;
+
+#define SysBase AA.sysBase
+#define DOSBase AA.dosBase
+
+BOOL inited = FALSE;
+
+static void strreplace(STRPTR target, CONST_STRPTR from, CONST_STRPTR to)
+{
+    STRPTR pos = strstr(target, from);
+    if (pos != NULL)
+    {
+        TEXT buff[512] = {0};
+        ULONG targetlen = strlen(target);
+        ULONG headlen = pos - target;
+        ULONG taillen = targetlen - strlen(from) - headlen;
+
+        // add head
+        strncat(buff, target, headlen);
+
+        // add to
+        strncat(buff, to, strlen(to));
+
+        // add tail
+        strncat(buff, pos + strlen(from), taillen);
+
+        // copy back
+        strncpy(target, buff, strlen(buff));
+        target[strlen(buff)] = 0;
+    }
+}
+
+static void init()
+{
+    if (inited)
+        return;
+
+    APTR (*fun)();
+    fun = dlsym(RTLD_DEFAULT, "__get_sysbase");
+
+    if (!fun)
+    {
+        printf("__get_sysbase not located\n");
+        return;
+    }
+
+    AA.sysBase = (struct ExecBase *)fun();
+
+    if (!AA.sysBase)
+    {
+        printf("SysBase not acquired\n");
+        return;
+    }
+
+    AA.dosBase = OpenLibrary("dos.library", 0L);
+
+    if (!AA.dosBase)
+    {
+        printf("DOSBase not acquired\n");
+        return;
+    }
+
+    inited = TRUE;
+    printf("Init completed\n");
+
+}
+
+FILE * fopen (const char * restrict pathname, const char * restrict mode)
+{
+    FILE *_return = NULL;
+    FILE *(*fun)(const char * restrict pathname, const char * restrict mode);
+    char lpathname[1024];
+
+    init();
+
+    /* Make copy */
+    strcpy(lpathname, pathname);
+
+    if (inited)
+    {
+        /* Get to path starting from ROOT: */
+        if (strstr(lpathname, "PROGDIR:") != NULL)
+        {
+            char progdir[1024];
+            NameFromLock(GetProgramDir(), progdir, 1024);
+            strcat(progdir, "/");
+            strreplace(lpathname, "PROGDIR:", progdir);
+        }
+    }
+
+    /* Convert path to Linux */
+    strreplace(lpathname, "ROOT:","/");
+    printf("path: %s\n", lpathname);
+
+    fun = dlsym(RTLD_NEXT, "fopen");
+    _return = fun(lpathname, mode);
+
+    return _return;
+}
