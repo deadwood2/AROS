15a16e640fa63e7cf354c0c2400ed5a1df4e11b2
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2019-06-22 14:12:25 +0200

    Introduce proxy string.h file to declare non standard functions.

---
+++ arch/all-runtime/clibs/stdc/include/string.h
@@ -0,0 +1,31 @@
+#ifndef AXRT_STRING_H
+#define AXRT_STRING_H
+
+#include "/usr/include/string.h"
+
+/* BSD/other UNIX */
+//size_t strlcpy(char *dst, const char *src, size_t size);
+//size_t strlcat(char *dst, const char *src, size_t size);
+//char *strsep(char **, const char *);
+char *strlwr(char *);
+//char *strupr(char *);
+char *strcasestr(const char * str, const char * search);
+
+/* SAS/C */
+//char * strrev(char * s);
+//int stch_l(const char * in, long * lvalue);
+//int stcd_l(const char * in, long * lvalue);
+//int stco_l(const char * in, long * lvalue);
+int stcl_d(char * out, long lvalue);
+//int stcl_h(char * out, long lvalue);
+//int stcl_o(char * out, long lvalue);
+//int stcu_d(char * out, unsigned uivalue);
+
+/* Supplemental (not std C) */
+//size_t stccpy(char *str1_ptr, const char *str2_ptr, size_t max);
+//char *stpsym(char *str_ptr, char *dest_ptr, int dest_size);
+//char * stpblk( const char * str );
+//#define stpbrk(s,cs) strpbrk(s,cs)
+//#define stpchr(s,c)  strchr(s,c)
+
+#endif
