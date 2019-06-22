34c9c05b375699d70a7db3ae290e7a465efbcc65
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2019-06-22 15:22:03 +0200

    Add more declarations to proxy header.
    It now matches functions exposed by stdc.library

--- arch/all-runtime/clibs/stdc/include/string.h
+++ arch/all-runtime/clibs/stdc/include/string.h
@@ -4,17 +4,16 @@
 #include "/usr/include/string.h"
 
 /* BSD/other UNIX */
-//size_t strlcpy(char *dst, const char *src, size_t size);
-//size_t strlcat(char *dst, const char *src, size_t size);
+size_t strlcpy(char *dst, const char *src, size_t size);
+size_t strlcat(char *dst, const char *src, size_t size);
 //char *strsep(char **, const char *);
 char *strlwr(char *);
 //char *strupr(char *);
-char *strcasestr(const char * str, const char * search);
 
 /* SAS/C */
 //char * strrev(char * s);
 //int stch_l(const char * in, long * lvalue);
-//int stcd_l(const char * in, long * lvalue);
+int stcd_l(const char * in, long * lvalue);
 //int stco_l(const char * in, long * lvalue);
 int stcl_d(char * out, long lvalue);
 //int stcl_h(char * out, long lvalue);
@@ -22,10 +21,17 @@ int stcl_d(char * out, long lvalue);
 //int stcu_d(char * out, unsigned uivalue);
 
 /* Supplemental (not std C) */
-//size_t stccpy(char *str1_ptr, const char *str2_ptr, size_t max);
+size_t stccpy(char *str1_ptr, const char *str2_ptr, size_t max);
 //char *stpsym(char *str_ptr, char *dest_ptr, int dest_size);
-//char * stpblk( const char * str );
+char * stpblk( const char * str );
 //#define stpbrk(s,cs) strpbrk(s,cs)
-//#define stpchr(s,c)  strchr(s,c)
+#define stpchr(s,c)  strchr(s,c)
+
+/* BSD */
+char *strcasestr(const char * str, const char * search);
+
+/* Extension */
+#define stricmp strcasecmp
+#define strnicmp strncasecmp
 
 #endif
