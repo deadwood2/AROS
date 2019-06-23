c6d72c1763f1df8d66b2737e3005620168437f19
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2019-06-23 12:13:50 +0200

    Add all SAC/C, BSD, suplement functions to stdc.library

--- arch/all-runtime/clibs/stdc/include/string.h
+++ arch/all-runtime/clibs/stdc/include/string.h
@@ -6,25 +6,25 @@
 /* BSD/other UNIX */
 size_t strlcpy(char *dst, const char *src, size_t size);
 size_t strlcat(char *dst, const char *src, size_t size);
-//char *strsep(char **, const char *);
+char *strsep(char **, const char *);
 char *strlwr(char *);
-//char *strupr(char *);
+char *strupr(char *);
 
 /* SAS/C */
-//char * strrev(char * s);
-//int stch_l(const char * in, long * lvalue);
+char * strrev(char * s);
+int stch_l(const char * in, long * lvalue);
 int stcd_l(const char * in, long * lvalue);
-//int stco_l(const char * in, long * lvalue);
+int stco_l(const char * in, long * lvalue);
 int stcl_d(char * out, long lvalue);
-//int stcl_h(char * out, long lvalue);
-//int stcl_o(char * out, long lvalue);
-//int stcu_d(char * out, unsigned uivalue);
+int stcl_h(char * out, long lvalue);
+int stcl_o(char * out, long lvalue);
+int stcu_d(char * out, unsigned uivalue);
 
 /* Supplemental (not std C) */
 size_t stccpy(char *str1_ptr, const char *str2_ptr, size_t max);
-//char *stpsym(char *str_ptr, char *dest_ptr, int dest_size);
+char *stpsym(char *str_ptr, char *dest_ptr, int dest_size);
 char * stpblk( const char * str );
-//#define stpbrk(s,cs) strpbrk(s,cs)
+#define stpbrk(s,cs) strpbrk(s,cs)
 #define stpchr(s,c)  strchr(s,c)
 
 /* BSD */
