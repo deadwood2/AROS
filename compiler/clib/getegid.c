025abadb813bb038a768687f4bed8303386fbcb8
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2021-02-21 15:20:44 +0100

    ABI_V0 compatibility: arosc.library - use comptible gid functions

---
+++ compiler/clib/getegid.c
@@ -0,0 +1,39 @@
+/*
+    Copyright © 2003-2013, The AROS Development Team. All rights reserved.
+    $Id$
+
+    POSIX.1-2008 function getegid().
+*/
+
+/*****************************************************************************
+
+    NAME */
+#include <unistd.h>
+
+	gid_t getegid(
+
+/*  SYNOPSIS */
+	void)
+
+/*  FUNCTION
+	
+    INPUTS
+	
+    RESULT
+	
+    NOTES
+        Always returns 0 for the moment
+
+    EXAMPLE
+
+    BUGS
+    	
+    SEE ALSO
+    	setgid()
+        
+    INTERNALS
+
+******************************************************************************/
+{
+    return 0;
+} /* getegid() */
