861e7d11ae4dbf82205b29feea7be3a1ac059e67
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2021-12-03 06:13:29 +0100

    Disabled posixc, introduce crtext

--- compiler/stdc/crtextfake.c
+++ compiler/stdc/crtextfake.c
@@ -8,6 +8,7 @@
 #include <signal.h>
 #include <setjmp.h>
 #include <time.h>
+#include <sys/time.h>
 
 const unsigned short int * const *__ctype_b_ptr = NULL;
 const unsigned char * const *__ctype_toupper_ptr = NULL;
@@ -24,6 +25,20 @@ void *realloc(void *oldmem, size_t newsize) { asm("int3");return NULL; }
 void *realloc_nocopy(void *oldmem, size_t newsize) { asm("int3");return NULL; }
 void *calloc(size_t count, size_t size) { asm("int3");return NULL; }
 int rand(void) { asm("int3");return 0; }
+void srand(unsigned int seed)  { asm("int3"); }
+time_t time(time_t *t) { asm("int3");return *t; }
+char *ctime(const time_t *t) { asm("int3");return NULL; }
+struct tm *gmtime(const time_t *t) { asm("int3");return NULL; }
+struct tm *localtime(const time_t *t) { asm("int3");return NULL; }
+clock_t clock(void) { clock_t c; asm("int3");return c; }
+char *strdup(const char *str) { asm("int3");return NULL; }
+int atexit(void (*func)(void)) { asm("int3");return 0; }
+void abort(void) { asm("int3"); }
+__sighandler_t *signal(int a, __sighandler_t * h) { asm("int3");return NULL; }
+char *strtok (char * str, const char * sep) { asm("int3");return NULL; }
+int gettimeofday(struct timeval * restrict tv, struct timezone * restrict tz) { asm("int3");return 0; }
+
+
 int remove(const char *filename) { asm("int3");return 0; }
 int rename(const char *old, const char *new) { asm("int3");return 0; }
 int __stdc_gmtoffset(void) { asm("int3");return 0; }
@@ -33,16 +48,6 @@ int *__stdc_geterrnoptr(void) { asm("int3");return NULL; }
 int *__stdc_set_errorptr(int *errorptr) { asm("int3");return NULL; }
 void __stdc_set_exitjmp(jmp_buf exitjmp, jmp_buf previousjmp) { asm("int3"); }
 char *__stdc_strerror (int n) { asm("int3");return NULL; }
-char *strdup(const char *str) { asm("int3");return NULL; }
-time_t time(time_t *t) { asm("int3");return *t; }
+
 int __stdc_ioerr2errno(int ioerr) { asm("int3");return 0; }
-void abort(void) { asm("int3"); }
-__sighandler_t *signal(int a, __sighandler_t * h) { asm("int3");return NULL; }
-clock_t clock(void) { clock_t c; asm("int3");return c; }
-char *ctime(const time_t *t) { asm("int3");return NULL; }
-void srand(unsigned int seed)  { asm("int3"); }
-struct tm *gmtime(const time_t *t) { asm("int3");return NULL; }
-char *strtok (char * str, const char * sep) { asm("int3");return NULL; }
-struct tm *localtime(const time_t *t) { asm("int3");return NULL; }
-int atexit(void (*func)(void)) { asm("int3");return 0; }
 
