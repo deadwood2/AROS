d0fd3513578141e4718e2f79fd144cc2f37d7cb5
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2021-12-28 09:37:48 +0100

    Add simple test for a variadic function

---
+++ developer/debug/test/crt/posixc/fprintf.c
@@ -0,0 +1,69 @@
+/*
+    Copyright (C) 2021, The AROS Development Team. All rights reserved.
+*/
+
+#include <stdio.h>
+#include "test.h"
+
+void args2()
+{
+    fprintf(stderr,
+        "%d %d\n",
+        1, 2);
+}
+
+void args4()
+{
+    fprintf(stderr,
+        "%d %d %d %d\n",
+        1, 2, 3, 4);
+}
+
+void args9()
+{
+    fprintf(stderr,
+        "%d %d %d %d %d %d %d %d %d\n",
+        1, 2, 3, 4, 5, 6, 7, 8, 9);
+}
+
+void args20()
+{
+    fprintf(stderr,
+        "%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n",
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+}
+
+void argsAll()
+{
+    fprintf(stderr,
+        "%d %d\n",
+        1, 2);
+
+    fprintf(stderr,
+        "%d %d %d %d\n",
+        1, 2, 3, 4);
+
+    fprintf(stderr,
+        "%d %d %d %d %d %d %d %d %d\n",
+        1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    fprintf(stderr,
+        "%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n",
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+}
+
+int main()
+{
+    args2();
+    args4();
+    args9();
+    args20();
+
+    argsAll();
+
+    return OK;
+}
+
+void cleanup()
+{
+}
