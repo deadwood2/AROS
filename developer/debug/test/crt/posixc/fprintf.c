98b06a5f2682983190a61a67b08ab93b4bfc73e6
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2022-01-02 11:20:39 +0100

    Update test to expose case matching ABI change

--- developer/debug/test/crt/posixc/fprintf.c
+++ developer/debug/test/crt/posixc/fprintf.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include "test.h"
 
+int __fprintf_CrtBase_stack_varargs_cnt = 18;
+
 void args2()
 {
     fprintf(stderr,
@@ -33,6 +35,13 @@ void args20()
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
 }
 
+void args22()
+{
+    fprintf(stderr,
+        "%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n",
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22);
+}
+
 void argsAll()
 {
     fprintf(stderr,
@@ -50,6 +59,10 @@ void argsAll()
     fprintf(stderr,
         "%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n",
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+
+    fprintf(stderr,
+        "%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n",
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22);
 }
 
 int main()
@@ -58,6 +71,7 @@ int main()
     args4();
     args9();
     args20();
+    args22();
 
     argsAll();
 
