a8f6b2d9fc5230395468ddbdfe31fb3c9c00cacf
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2022-02-03 17:33:37 +0100

    Adjust to C library changes

---
+++ arch/all-runtime/clibs/stdlib/stdlib_intern.h
@@ -0,0 +1,17 @@
+/*
+    Copyright (C) 2022, The AROS Development Team. All rights reserved.
+*/
+
+#ifndef STDLIB_INTERN_H
+#define STDLIB_INTERN_H
+
+#ifndef EXEC_LIBRARIES_H
+#   include <exec/libraries.h>
+#endif
+
+struct StdlibBase
+{
+    struct Library  base;
+};
+
+#endif
