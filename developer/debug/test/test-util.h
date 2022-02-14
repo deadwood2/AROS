5955d59b2033b26494621ffe6ad33d0ac34ac96e
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2022-02-14 21:30:12 +0100

    Add shared test definitions header

---
+++ developer/debug/test/test-util.h
@@ -0,0 +1,10 @@
+/*
+    Copyright (C) 2022, The AROS Development Team. All rights reserved.
+*/
+
+#ifndef TEST_UTIL_H
+#define TEST_UTIL_H
+
+#define CUNIT_ABSOLUTE_PATH "SYS:Development/Debug/Tests/cunit"
+
+#endif
