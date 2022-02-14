5eab62d10b3b003657b75bbc8b5c8dd8b76aebf1
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2022-02-14 07:25:38 +0100

    Update test to handle more cases. Add missing TAG_DONE.

--- developer/debug/test/dos/clicreatenewproc.c
+++ developer/debug/test/dos/clicreatenewproc.c
@@ -18,28 +18,76 @@ typedef struct RAWARG_s { } *RAWARG;
 This is output of the test from AmigaOS 3.1 Shell
 :> stack 50000
 :> clicreatenewproc
-Main process, stack: 3200, cli_stack 50000
+Main, tc_SPUpper/SPLower:  50000, pr_StackSize:   3200, cli_DefaultStack:  50000
+
 Creating subprocess, no stack set, no cli
-Subprocess, stack: 4000, cli_stack -1
-Creating subprocess, stack set to 64000, no cli
-Subprocess, stack: 64000, cli_stack -1
-Creating subprocess, stack set to 64000, cli
-Subprocess, stack: 64000, cli_stack 64000
+Sub , tc_SPUpper/SPLower:   4000, pr_StackSize:   4000, cli_DefaultStack:     -1
+
 Creating subprocess, no stack set, cli
-Subprocess, stack: 4000, cli_stack 4000
+Sub , tc_SPUpper/SPLower:   4000, pr_StackSize:   4000, cli_DefaultStack:   4000
+
+------------------------------------------------------
+Creating subprocess, stack set to 64000, no cli
+Sub , tc_SPUpper/SPLower:  64000, pr_StackSize:  64000, cli_DefaultStack:     -1
+
+Main, tc_SPUpper/SPLower:  50000, pr_StackSize:   3200, cli_DefaultStack:  50000
+
+Creating subprocess, no stack set, no cli
+Sub , tc_SPUpper/SPLower:   4000, pr_StackSize:   4000, cli_DefaultStack:     -1
+
+Creating subprocess, no stack set, cli
+Sub , tc_SPUpper/SPLower:   4000, pr_StackSize:   4000, cli_DefaultStack:   4000
+
+------------------------------------------------------
+Creating subprocess, stack set to 96000, cli
+Sub , tc_SPUpper/SPLower:  96000, pr_StackSize:  96000, cli_DefaultStack:  96000
+
+Main, tc_SPUpper/SPLower:  50000, pr_StackSize:   3200, cli_DefaultStack:  50000
+
+Creating subprocess, no stack set, no cli
+Sub , tc_SPUpper/SPLower:   4000, pr_StackSize:   4000, cli_DefaultStack:     -1
+
+Creating subprocess, no stack set, cli
+Sub , tc_SPUpper/SPLower:   4000, pr_StackSize:   4000, cli_DefaultStack:   4000
+
+------------------------------------------------------
 
 This is output of the test from AROS
 :> stack 50000
 :> clicreatenewproc
-Main process, stack: 262144, cli_stack 50000
+Main, tc_SPUpper/SPLower:  50000, pr_StackSize: 262144, cli_DefaultStack:  50000
+
 Creating subprocess, no stack set, no cli
-Subprocess, stack: 50000, cli_stack -1
-Creating subprocess, stack set to 64000, no cli
-Subprocess, stack: 64000, cli_stack -1
-Creating subprocess, stack set to 64000, cli
-Subprocess, stack: 64000, cli_stack 64000
+Sub , tc_SPUpper/SPLower:  50000, pr_StackSize:  50000, cli_DefaultStack:     -1
+
 Creating subprocess, no stack set, cli
-Subprocess, stack: 50000, cli_stack 50000
+Sub , tc_SPUpper/SPLower:  50000, pr_StackSize:  50000, cli_DefaultStack:  50000
+
+------------------------------------------------------
+Creating subprocess, stack set to 64000, no cli
+Sub , tc_SPUpper/SPLower:  64000, pr_StackSize:  64000, cli_DefaultStack:     -1
+
+Main, tc_SPUpper/SPLower:  50000, pr_StackSize: 262144, cli_DefaultStack:  50000
+
+Creating subprocess, no stack set, no cli
+Sub , tc_SPUpper/SPLower:  50000, pr_StackSize:  50000, cli_DefaultStack:     -1
+
+Creating subprocess, no stack set, cli
+Sub , tc_SPUpper/SPLower:  50000, pr_StackSize:  50000, cli_DefaultStack:  50000
+
+------------------------------------------------------
+Creating subprocess, stack set to 96000, cli
+Sub , tc_SPUpper/SPLower:  96000, pr_StackSize:  96000, cli_DefaultStack:  96000
+
+Main, tc_SPUpper/SPLower:  50000, pr_StackSize: 262144, cli_DefaultStack:  50000
+
+Creating subprocess, no stack set, no cli
+Sub , tc_SPUpper/SPLower:  50000, pr_StackSize:  50000, cli_DefaultStack:     -1
+
+Creating subprocess, no stack set, cli
+Sub , tc_SPUpper/SPLower:  50000, pr_StackSize:  50000, cli_DefaultStack:  50000
+
+------------------------------------------------------
 
 On AROS, unless NP_StackStack size is provided, child processes inherit stack
 from parent process cli_DefaultStack if available.
@@ -51,11 +99,12 @@ static struct Task *task;
 
 static void __printf_info(struct Process *p, struct CommandLineInterface *cli)
 {
-    LONG argl[2];
-    argl[0] = p->pr_StackSize;
-    argl[1] = cli ? cli->cli_DefaultStack * CLI_DEFAULTSTACK_UNIT: -1;
+    LONG argl[3];
+    argl[0] = (LONG)(((UBYTE *)p->pr_Task.tc_SPUpper) - ((UBYTE *)p->pr_Task.tc_SPLower));
+    argl[1] = p->pr_StackSize;
+    argl[2] = cli ? cli->cli_DefaultStack * CLI_DEFAULTSTACK_UNIT: -1;
 
-    VPrintf(", stack: %ld, cli_stack %ld\n", (RAWARG)argl);
+    VPrintf(", tc_SPUpper/SPLower: %6ld, pr_StackSize: %6ld, cli_DefaultStack: %6ld\n\n", (RAWARG)argl);
 }
 static void SubProcess(void)
 {
@@ -64,9 +113,24 @@ static void SubProcess(void)
 
     me = (struct Process *)FindTask(NULL);
     cli = Cli();
-    VPrintf("Subprocess", NULL);__printf_info(me, cli);
+    VPrintf("Sub ", NULL);__printf_info(me, cli);
 
     Signal(task, 1<<sig);
+}
+
+static void NoStack_NoCli_Cli()
+{
+
+    VPrintf("Creating subprocess, no stack set, no cli\n", NULL);
+    CreateNewProcTags(NP_Entry, (IPTR) SubProcess, NP_Output, Output(), NP_CloseOutput, FALSE, TAG_DONE);
+    Wait(1<<sig);
+
+    VPrintf("Creating subprocess, no stack set, cli\n", NULL);
+    CreateNewProcTags(NP_Entry, (IPTR) SubProcess, NP_Output, Output(), NP_CloseOutput, FALSE,
+        NP_Cli, TRUE, TAG_DONE);
+    Wait(1<<sig);
+
+    VPrintf("------------------------------------------------------\n", NULL);
 }
 
 int main(void)
@@ -80,26 +144,27 @@ int main(void)
     me = (struct Process *)task;
     cli = Cli();
 
-    VPrintf("Main process", NULL);__printf_info(me, cli);
+    VPrintf("Main", NULL);__printf_info(me, cli);
 
-    VPrintf("Creating subprocess, no stack set, no cli\n", NULL);
-    CreateNewProcTags(NP_Entry, (IPTR) SubProcess, NP_Output, Output(), NP_CloseOutput, FALSE);
-    Wait(1<<sig);
+    NoStack_NoCli_Cli();
 
     VPrintf("Creating subprocess, stack set to 64000, no cli\n", NULL);
     CreateNewProcTags(NP_Entry, (IPTR) SubProcess, NP_Output, Output(), NP_CloseOutput, FALSE,
-        NP_StackSize, 64000);
+        NP_StackSize, 64000, TAG_DONE);
     Wait(1<<sig);
 
-    VPrintf("Creating subprocess, stack set to 64000, cli\n", NULL);
+    VPrintf("Main", NULL);__printf_info(me, cli);
+
+    NoStack_NoCli_Cli();
+
+    VPrintf("Creating subprocess, stack set to 96000, cli\n", NULL);
     CreateNewProcTags(NP_Entry, (IPTR) SubProcess, NP_Output, Output(), NP_CloseOutput, FALSE,
-        NP_StackSize, 64000, NP_Cli, TRUE);
+        NP_StackSize, 96000, NP_Cli, TRUE, TAG_DONE);
     Wait(1<<sig);
 
-    VPrintf("Creating subprocess, no stack set, cli\n", NULL);
-    CreateNewProcTags(NP_Entry, (IPTR) SubProcess, NP_Output, Output(), NP_CloseOutput, FALSE,
-        NP_Cli, TRUE);
-    Wait(1<<sig);
+    VPrintf("Main", NULL);__printf_info(me, cli);
+
+    NoStack_NoCli_Cli();
 
     FreeSignal(sig);
 
