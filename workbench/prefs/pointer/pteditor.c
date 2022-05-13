fbd977f743ab03c02f2b1abfdb26fb92d954abbd
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2022-05-13 21:33:14 +0200

    Backport: Fix: tags need to finish with TAG_DONE
    Fixes crash visible when building with -O2

--- workbench/prefs/pointer/pteditor.c
+++ workbench/prefs/pointer/pteditor.c
@@ -87,7 +87,8 @@ AROS_UFHA(APTR, msg, A1))
         MUIA_PPreview_FileName, pointerprefs[entry].filename,
         MUIA_PPreview_Alpha,    pointerprefs[entry].npp.npp_AlphaValue,
         MUIA_PPreview_HSpotX,   - pointerprefs[entry].npp.npp_X,
-        MUIA_PPreview_HSpotY,   - pointerprefs[entry].npp.npp_Y
+        MUIA_PPreview_HSpotY,   - pointerprefs[entry].npp.npp_Y,
+        TAG_DONE
     );
 
     data->pded_oldentry = entry;
@@ -240,7 +241,8 @@ STATIC VOID PTPrefs2Gadgets(struct PTEditor_DATA *data)
         MUIA_PPreview_FileName, pointerprefs[entry].filename,
         MUIA_PPreview_Alpha,    pointerprefs[entry].npp.npp_AlphaValue,
         MUIA_PPreview_HSpotX,   - pointerprefs[entry].npp.npp_X,
-        MUIA_PPreview_HSpotY,   - pointerprefs[entry].npp.npp_Y
+        MUIA_PPreview_HSpotY,   - pointerprefs[entry].npp.npp_Y,
+        TAG_DONE
     );
 }
 
