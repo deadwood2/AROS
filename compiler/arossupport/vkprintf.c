e01527ebf705ebd536a4664d4671da380282f998
Kalamatee <kalamatee@gmail.com>
2022-05-21 15:56:40 +0100

    split out vkprintf from kprintf.

---
+++ compiler/arossupport/vkprintf.c
@@ -0,0 +1,25 @@
+/*
+    Copyright (C) 2022, The AROS Development Team. All rights reserved.
+
+    Desc: Formats a message and makes sure the user will see it.
+*/
+
+#include <aros/config.h>
+#include <aros/arossupportbase.h>
+#include <stdarg.h>
+
+#include <aros/system.h>
+#include <proto/exec.h>
+#include <proto/arossupport.h>
+#undef vkprintf
+#include <exec/execbase.h>
+
+#include <proto/arossupport.h>
+
+extern int _vkprintf(const char * format, va_list args);
+
+/******************************************************************************/
+int vkprintf (const char * format, va_list args)
+{
+  return _vkprintf (format, args);
+} /* vkprintf */
