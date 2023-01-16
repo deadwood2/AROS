ae6283821cc65d5a058339c49f44154c1e9b28b0
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2023-01-16 12:46:14 +0100

    ABI_V0 compatibility: muimaster.library offsets and struct EventHandlerNode
    Note: change in
    betterstring/include/libraries/muiaros.h has been removed from patchset
    as the header was removed. It may return with newer versions, pay attention

--- workbench/classes/zune/nlist/include/libraries/muiaros.h
+++ workbench/classes/zune/nlist/include/libraries/muiaros.h
@@ -1165,7 +1165,26 @@ struct MUI_RenderInfo
  MUI_EventHandlerNode as used by
  MUIM_Window_AddEventHandler/RemoveEventHandler
 **************************************************************************/
+/* ABI_V0 compatibility */
+#ifdef __AROS__
+#if defined(__i386__)
+/* AROS Nodes are not necessarily binary compatible with AOS ones, so
+ * the (MUI_EventHandlerNode *) -> (Node *) cast hack doesnt work.
+ */
+struct MUI_EventHandlerNode
+{
+    struct Node    ehn_Node;     /* embedded node structure, private! */
+    UWORD          ehn_Flags;    /* some flags, see below */
+    Object        *ehn_Object;   /* object which should receive MUIM_HandleEvent. */
+    struct IClass *ehn_Class;    /* Class for CoerceMethod(). If NULL DoMethod() is used */
+    ULONG          ehn_Events;   /* the IDCMP flags the handler should be invoked. */
+    BYTE           ehn_Priority; /* sorted by priority. */
+};
+#define MUI_EVENTHANDLERNODE_DEFINED
+#endif
+#endif
 
+#ifndef MUI_EVENTHANDLERNODE_DEFINED
 struct MUI_EventHandlerNode
 {
     struct MinNode ehn_Node;     /* embedded node structure, private! */
@@ -1176,6 +1195,9 @@ struct MUI_EventHandlerNode
     struct IClass *ehn_Class;    /* Class for CoerceMethod(). If NULL DoMethod() is used */
     ULONG          ehn_Events;   /* the IDCMP flags the handler should be invoked. */
 };
+#endif
+
+#undef MUI_EVENTHANDLERNODE_DEFINED
 
 /* here are the flags for ehn_Flags */
 #define MUI_EHF_ALWAYSKEYS  (1<<0)
