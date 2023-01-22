be19907de5f433c7406ff873392341b66c5624ae
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2023-01-22 13:32:03 +0100

    ABI_V0 compatibility: arosc.library - adjust to 64-bit file changes

--- compiler/clib/__fdesc.h
+++ compiler/clib/__fdesc.h
@@ -26,6 +26,8 @@ typedef struct _fcb
 #define _FCB_DONTCLOSE_FH ((unsigned int)1<<1)
 #define _FCB_FLUSHONREAD  ((unsigned int)1<<2)
 #define _FCB_CONSOLERAW   ((unsigned int)1<<3)
+#define _FCB_FH64         ((unsigned int)1<<14)
+#define _FCB_FS64         ((unsigned int)1<<15)
 
 #define FLUSHONREADCHECK                                                \
     if (__builtin_expect(fdesc->fcb->privflags & _FCB_FLUSHONREAD, 0))  \
