ed4a17d54eb34820da5d401672dd388769cd30f4
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2023-11-01 14:46:18 +0100

    ABI_V0 compatibility: re-introduce Mesa 7
    Adjustments to supporting libraries and drivers

--- workbench/devs/monitors/IntelGMA/intelgma_startup.c
+++ workbench/devs/monitors/IntelGMA/intelgma_startup.c
@@ -67,7 +67,7 @@ static const struct OOP_ABDescr attrbases[] =
     {NULL, NULL }
 };
 
-const TEXT version_string[] = "$VER: IntelGMA 3.13 (17.10.2016)\n";
+const TEXT version_string[] = "$VER: IntelGMA 3.14 (7.1.2023)\n";
 
 extern struct WBStartup *WBenchMsg;
 int __nocommandline = 1;
