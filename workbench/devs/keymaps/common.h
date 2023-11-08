9b64c24f04392fe2e579e4c586ce1a2d1086a295
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2023-11-08 18:40:44 +0100

    Other: Bring back building old ELF keymap where HUNK one is not available
    Note: they are placed in Sys:Storage/Keymaps only because otherwise would
    confuse make.

---
+++ workbench/devs/keymaps/common.h
@@ -0,0 +1,75 @@
+/*
+    Copyright (C) 2013, The AROS Development Team. All rights reserved.
+
+*/
+
+#include <devices/keymap.h>
+
+/* ABI_V0 compatibility */
+#if defined(__i386__)
+
+#define DEFINE_KEYMAP(kmname)                   \
+                                                 \
+STATIC char  keymapname[] = kmname;             \
+                                                \
+STATIC CONST UBYTE lokeymaptypes[];             \
+STATIC CONST IPTR  lokeymap[];                  \
+STATIC CONST UBYTE locapsable[];                \
+STATIC CONST UBYTE lorepeatable[];              \
+                                                \
+STATIC CONST UBYTE hikeymaptypes[];             \
+STATIC CONST IPTR  hikeymap[];                  \
+STATIC CONST UBYTE hicapsable[];                \
+STATIC CONST UBYTE hirepeatable[];              \
+                                                \
+CONST struct KeyMapNode km =                    \
+{                                               \
+    {                                           \
+        NULL, NULL, keymapname, 0, 0            \
+    },                                          \
+    {                                           \
+        (UBYTE *)lokeymaptypes,                 \
+        (IPTR  *)lokeymap,                      \
+        (UBYTE *)locapsable,                    \
+        (UBYTE *)lorepeatable,                  \
+        (UBYTE *)hikeymaptypes,                 \
+        (IPTR  *)hikeymap,                      \
+        (UBYTE *)hicapsable,                    \
+        (UBYTE *)hirepeatable                   \
+    }                                           \
+};
+
+#else
+
+#define DEFINE_KEYMAP(kmname)                   \
+                                                 \
+STATIC char  keymapname[] = kmname;             \
+                                                \
+STATIC CONST UBYTE lokeymaptypes[];             \
+STATIC CONST IPTR  lokeymap[];                  \
+STATIC CONST UBYTE locapsable[];                \
+STATIC CONST UBYTE lorepeatable[];              \
+                                                \
+STATIC CONST UBYTE hikeymaptypes[];             \
+STATIC CONST IPTR  hikeymap[];                  \
+STATIC CONST UBYTE hicapsable[];                \
+STATIC CONST UBYTE hirepeatable[];              \
+                                                \
+CONST struct KeyMapNode km =                    \
+{                                               \
+    {                                           \
+        NULL, NULL, 0, 0, keymapname            \
+    },                                          \
+    {                                           \
+        (UBYTE *)lokeymaptypes,                 \
+        (IPTR  *)lokeymap,                      \
+        (UBYTE *)locapsable,                    \
+        (UBYTE *)lorepeatable,                  \
+        (UBYTE *)hikeymaptypes,                 \
+        (IPTR  *)hikeymap,                      \
+        (UBYTE *)hicapsable,                    \
+        (UBYTE *)hirepeatable                   \
+    }                                           \
+};
+
+#endif
