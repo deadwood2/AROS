d158e46c29712fece60268c8e0fddd3a722f3c40
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2023-11-29 15:08:07 +0100

    Rework autoopen test to standart approach

--- developer/debug/test/library/cunit-library-autoopen.c
+++ developer/debug/test/library/cunit-library-autoopen.c
@@ -8,78 +8,42 @@
 #include <proto/dos.h>
 #include <proto/single.h>
 
-#include <CUnit/Basic.h>
-#include <CUnit/Automated.h>
+#include <CUnit/CUnitCI.h>
 
-/* The suite initialization function.
-  * Returns zero on success, non-zero otherwise.
- */
-int init_suite(void)
+CU_SUITE_SETUP()
 {
-    return 0;
+    return CUE_SUCCESS;
 }
 
-/* The suite cleanup function.
-  * Returns zero on success, non-zero otherwise.
- */
-int clean_suite(void)
+CU_SUITE_TEARDOWN()
 {
-    return 0;
+    return CUE_SUCCESS;
 }
 
-void testADD(void)
+CU_TEST_SETUP()
+{
+}
+
+CU_TEST_TEARDOWN()
+{
+}
+
+void test_add(void)
 {
     RegSetValue(5);
 
-    const LONG e1 = 15;
     LONG r1 = RegAdd4(1, 2, 3, 4);
 
-    if (e1 != r1)
-    {
-        CU_FAIL("15 != RegAdd4(1, 2, 3, 4)");
-    }
+    CU_ASSERT_EQUAL(15, r1);
 
-    const int e5 = 26;
     int r5 = StackAdd4OrMore(6, 1, 2, 3, 4, 5, 6);
-    if (e5 != r5)
-    {
-        CU_FAIL("26 != StackAdd4OrMore(6, 1, 2, 3, 4, 5, 6)");
-    }
 
-    CU_PASS("");
+    CU_ASSERT_EQUAL(26, r5);
 }
 
-int main(void)
+int main(int argc, char** argv)
 {
-    CU_pSuite pSuite = NULL;
-
-    /* initialize the CUnit test registry */
-    if (CUE_SUCCESS != CU_initialize_registry())
-        return CU_get_error();
-
-   /* add a suite to the registry */
-    pSuite = CU_add_suite("AutoOpen_Suite", init_suite, clean_suite);
-    if (NULL == pSuite) {
-        CU_cleanup_registry();
-        return CU_get_error();
-    }
-
-   /* add the tests to the suite */
-    if ((NULL == CU_add_test(pSuite, "test of calling auto-opened single.library", testADD)))
-    {
-        CU_cleanup_registry();
-        return CU_get_error();
-    }
-
-    /* Run all tests using the CUnit Basic & Automated interfaces */
-    CU_basic_set_mode(CU_BRM_VERBOSE);
-    CU_basic_run_tests();
-    CU_basic_set_mode(CU_BRM_SILENT);
-    CU_automated_package_name_set("GenmoduleUnitTests");
-    CU_set_output_filename("Genmodule-AutoOpen");
-    CU_automated_enable_junit_xml(CU_TRUE);
-    CU_automated_run_tests();
-    CU_cleanup_registry();
-
-    return CU_get_error();
+    CU_CI_DEFINE_SUITE("Library_AutoOpen_Suite", __cu_suite_setup, __cu_suite_teardown, __cu_test_setup, __cu_test_teardown);
+    CUNIT_CI_TEST(test_add);
+    return CU_CI_RUN_SUITES();
 }
