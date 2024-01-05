e97b2f6578ee1cd913022ffd2b9284aea0cfc1e4
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2024-01-05 13:56:43 +0100

    Update revision

--- arch/all-runtime/axrt/startup/rt_version.h
+++ arch/all-runtime/axrt/startup/rt_version.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2020-2022, The AROS Development Team.
+    Copyright (C) 2020-2024, The AROS Development Team.
 */
 
 #ifndef RT_VERSION_H
@@ -8,7 +8,7 @@
 #define RT_STARTUP  (6)
 #define RT_ABI      (4)
 #define RT_VERSION  (41)
-#define RT_REVISION (6)
+#define RT_REVISION (7)
 
 #define RT_VER      ((RT_STARTUP<<28)|(RT_ABI<<24)|(RT_VERSION<<8)|(RT_REVISION<<0))
 
