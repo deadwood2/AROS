be867b781a32beae2f586fea5a3da673a6442da0
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2024-01-19 17:04:46 +0100

    Other: Adjust runtests.sh to ABIv0 branch

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -11,10 +11,10 @@ check_location ()
 
 check_location
 
-AROSBUILDDIR=$(pwd)/core-linux-x86_64-tests
+AROSBUILDDIR=$(pwd)/alt-abiv0-linux-i386-tests
 
 AROSSRCDIR=$(pwd)/AROS
-AROSRUNDIR=$AROSBUILDDIR/bin/linux-x86_64/AROS
+AROSRUNDIR=$AROSBUILDDIR/bin/linux-i386/AROS
 AROSLOGDIR=$AROSBUILDDIR/logs
 
 if [[ $1 == 'clean' ]]
@@ -24,19 +24,19 @@ fi
 
 mkdir -p $AROSRUNDIR
 mkdir -p $AROSLOGDIR
-rsync -r -t -l --info=progress2 -s ./core-linux-x86_64-d/bin/linux-x86_64/AROS/* $AROSRUNDIR
-cp ./core-linux-x86_64-d/bin/linux-x86_64/AROS/.gdb_ignore_errors.py $AROSRUNDIR
-cp ./core-linux-x86_64-d/bin/linux-x86_64/AROS/.gdbinit $AROSRUNDIR
+rsync -r -t -l --info=progress2 -s ./alt-abiv0-linux-i386-d/bin/linux-i386/AROS/* $AROSRUNDIR
+cp ./alt-abiv0-linux-i386-d/bin/linux-i386/AROS/.gdb_ignore_errors.py $AROSRUNDIR
+cp ./alt-abiv0-linux-i386-d/bin/linux-i386/AROS/.gdbinit $AROSRUNDIR
 
 cp $AROSSRCDIR/scripts/nightly/autotest/User-Startup.cunit $AROSRUNDIR/S/User-Startup
 cp $AROSSRCDIR/scripts/nightly/autotest/Try $AROSRUNDIR/S/
 cp $AROSSRCDIR/scripts/nightly/autotest/Test.cunit $AROSRUNDIR/S/Test
 mv $AROSRUNDIR/Devs/Monitors/X1* $AROSRUNDIR/Storage/Monitors
 sed -i "s|Startup = 0|Startup = 1|g" $AROSRUNDIR/Devs/DOSDrivers/DEBUG
-sed -i "s|module Devs/Drivers/gfx.hidd|&\nmodule Storage/Drivers/headlessgfx.hidd|g" $AROSRUNDIR/boot/linux/AROSBootstrap.conf
-sed -i "/module Devs\/Drivers\/x11gfx.hidd/d" $AROSRUNDIR/boot/linux/AROSBootstrap.conf
+sed -i "s|module Devs/Drivers/gfx.hidd|&\nmodule Storage/Drivers/headlessgfx.hidd|g" $AROSRUNDIR/Arch/linux/AROSBootstrap.conf
+sed -i "/module Devs\/Drivers\/x11gfx.hidd/d" $AROSRUNDIR/Arch/linux/AROSBootstrap.conf
 cd $AROSRUNDIR
-./boot/linux/AROSBootstrap
+./Arch/linux/AROSBootstrap
 cd ../../../../
 find $AROSRUNDIR/ -name "*-Results.xml" -exec cp {} $AROSLOGDIR/ \;
 $AROSSRCDIR/scripts/merge_junit_results.py $AROSLOGDIR > $AROSLOGDIR/all-unittests.xml
