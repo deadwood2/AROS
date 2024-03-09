f02e26fd87ca2b2f917501f34450a7efe91ea277
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2024-03-09 15:58:37 +0100

    Move assembler macros to separate header for re-use

--- workbench/tools/AoA/AoA.c
+++ workbench/tools/AoA/AoA.c
@@ -8,49 +8,13 @@
 #include <string.h>
 
 #include "exec/functions.h"
+#include "aros/asm.h"
 
 BPTR LoadSeg32 (CONST_STRPTR name, struct DosLibrary *DOSBase);
 
 struct DosLibraryV0 *abiv0DOSBase;
 
-#define EXTER_PROXY(name)       \
-__asm__ volatile(               \
-    "   .code32\n"              \
-    "proxy_" #name ":\n"
 
-#define LEAVE_PROXY             \
-    "   ret\n"                  \
-    "   .code64\n"              \
-    ::);
-
-#define ENTER64                 \
-    "   push $0x33\n"           \
-    "   lea 1f, %%ecx\n"        \
-    "   push %%ecx\n"           \
-    "   lret\n"                 \
-    "   .code64\n"              \
-    "1:\n"
-
-#define ENTER32                 \
-    "   subq $8, %%rsp\n"       \
-    "   movl $0x23, 4(%%rsp)\n" \
-    "   lea 2f,%%ecx\n"         \
-    "   movl %%ecx, (%%rsp)\n"  \
-    "   lret\n"                 \
-    "   .code32\n"              \
-    "2:\n"
-
-#define CALL_IMPL64(name)       \
-    "   call abiv0_" #name "\n"
-
-#define COPY_ARG_1              \
-    "   movl 4(%%rsp), %%edi\n"
-
-#define COPY_ARG_3              \
-    "   movl 8(%%rsp), %%esi\n"
-
-#define COPY_ARG_2              \
-    "   movl 12(%%rsp), %%edx\n"
 
 APTR abiv0_AllocMem(ULONG byteSize, ULONG requirements, struct ExecBaseV0 *SysBaseV0)
 {
---
+++ workbench/tools/AoA/abiv0/include/aros/asm.h
@@ -0,0 +1,43 @@
+#ifndef AROS_ASM_H
+#define AROS_ASM_H
+
+#define EXTER_PROXY(name)       \
+__asm__ volatile(               \
+    "   .code32\n"              \
+    "proxy_" #name ":\n"
+
+#define LEAVE_PROXY             \
+    "   ret\n"                  \
+    "   .code64\n"              \
+    ::);
+
+#define ENTER64                 \
+    "   push $0x33\n"           \
+    "   lea 1f, %%ecx\n"        \
+    "   push %%ecx\n"           \
+    "   lret\n"                 \
+    "   .code64\n"              \
+    "1:\n"
+
+#define ENTER32                 \
+    "   subq $8, %%rsp\n"       \
+    "   movl $0x23, 4(%%rsp)\n" \
+    "   lea 2f,%%ecx\n"         \
+    "   movl %%ecx, (%%rsp)\n"  \
+    "   lret\n"                 \
+    "   .code32\n"              \
+    "2:\n"
+
+#define CALL_IMPL64(name)       \
+    "   call abiv0_" #name "\n"
+
+#define COPY_ARG_1              \
+    "   movl 4(%%rsp), %%edi\n"
+
+#define COPY_ARG_3              \
+    "   movl 8(%%rsp), %%esi\n"
+
+#define COPY_ARG_2              \
+    "   movl 12(%%rsp), %%edx\n"
+
+#endif
