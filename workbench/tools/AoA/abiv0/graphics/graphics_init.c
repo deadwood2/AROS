0ce314f0ecec94f36d8b5604bdcbd663e7f54e7f
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2024-03-23 16:47:15 +0100

    Refactor: move graphics code to seperate file

---
+++ workbench/tools/AoA/abiv0/graphics/graphics_init.c
@@ -0,0 +1,103 @@
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <proto/graphics.h>
+#include <aros/debug.h>
+
+
+#include "../include/exec/structures.h"
+#include "../include/exec/functions.h"
+#include "../include/aros/cpu.h"
+#include "../include/aros/proxy.h"
+#include "../include/graphics/structures.h"
+#include "../include/graphics/proxy_structures.h"
+#include "../include/utility/structures.h"
+
+extern struct ExecBaseV0 *abiv0SysBase;
+
+struct LibraryV0 *abiv0_Gfx_OpenLib(ULONG version, struct LibraryV0 *GfxBaseV0)
+{
+    return GfxBaseV0;
+}
+MAKE_PROXY_ARG_2(Gfx_OpenLib)
+
+ULONG abiv0_GetBitMapAttr(struct BitMapV0 *bitmap, ULONG attribute, struct LibraryV0 *GfxBaseV0)
+{
+bug("abiv0_GetBitMapAttr: STUB\n");
+    if (attribute == BMA_DEPTH)
+    {
+        return 24;
+    }
+asm("int3");
+}
+MAKE_PROXY_ARG_3(GetBitMapAttr)
+
+void abiv0_GetRGB32(struct ColorMapV0 * cm, ULONG firstcolor, ULONG ncolors, ULONG *table, struct LibraryV0 *GfxBaseV0)
+{
+    struct ColorMapProxy *proxy = (struct ColorMapProxy *)cm;
+    return GetRGB32(proxy->native, firstcolor, ncolors, table);
+}
+MAKE_PROXY_ARG_5(GetRGB32)
+
+APTR abiv0_OpenFont(APTR textAttr, struct LibraryV0 *GfxBaseV0)
+{
+bug("abiv0_OpenFont: STUB\n");
+    return NULL;
+}
+MAKE_PROXY_ARG_3(OpenFont)
+
+LONG abiv0_ObtainBestPenA(struct ColorMapV0 *cm, ULONG r, ULONG g, ULONG b, struct TagItemV0 *tags, struct LibraryV0 *GfxBaseV0)
+{
+    struct ColorMapProxy *proxy = (struct ColorMapProxy *)cm;
+
+    if (tags == NULL)
+    {
+        return ObtainBestPenA(proxy->native, r, g, b, NULL);
+    }
+    else
+    {
+        if (tags[0].ti_Tag == OBP_FailIfBad)
+        {
+            struct TagItem tagtmp[] =
+            {
+                { OBP_FailIfBad, FALSE },
+                { TAG_DONE, 0L}
+            };
+
+            tagtmp[0].ti_Data = tags[0].ti_Data;
+            return ObtainBestPenA(proxy->native, r, g, b, tagtmp);
+        }
+        else
+        {
+asm("int3");
+        }
+    }
+    return 0;
+}
+MAKE_PROXY_ARG_6(ObtainBestPenA)
+
+struct LibraryV0 *shallow_InitResident32(struct ResidentV0 *resident, BPTR segList, struct ExecBaseV0 *SysBaseV0);
+BPTR LoadSeg32 (CONST_STRPTR name, struct DosLibrary *DOSBase);
+struct ResidentV0 * findResident(BPTR seg, CONST_STRPTR name);
+APTR abiv0_DOS_OpenLibrary(CONST_STRPTR name, ULONG version, struct ExecBaseV0 *SysBaseV0);
+
+void init_graphics()
+{
+    BPTR graphicsseg = LoadSeg32("SYS:Libs32/partial/graphics.library", DOSBase);
+    struct ResidentV0 *graphicsres = findResident(graphicsseg, NULL);
+    struct GfxBaseV0 *abiv0GfxBase = (struct GfxBaseV0 *)shallow_InitResident32(graphicsres, graphicsseg, abiv0SysBase);
+    /* Remove all vectors for now */
+    const ULONG graphicsjmpsize = 202 * sizeof(APTR32);
+    APTR32 *graphicsjmp = AllocMem(graphicsjmpsize, MEMF_CLEAR);
+    CopyMem((APTR)abiv0GfxBase - graphicsjmpsize, graphicsjmp, graphicsjmpsize);
+    for (int i = 1; i <= 201; i++) __AROS_SETVECADDRV0(abiv0GfxBase, i, 0);
+    abiv0GfxBase->ExecBase = (APTR32)(IPTR)abiv0SysBase;
+    *(ULONG *)((IPTR)abiv0GfxBase + 0x4b0) = (APTR32)(IPTR)abiv0_DOS_OpenLibrary("utility.library", 0L, abiv0SysBase);
+
+    __AROS_SETVECADDRV0(abiv0GfxBase,   1, (APTR32)(IPTR)proxy_Gfx_OpenLib);
+    __AROS_SETVECADDRV0(abiv0GfxBase,  12, (APTR32)(IPTR)proxy_OpenFont);
+    __AROS_SETVECADDRV0(abiv0GfxBase, 160, (APTR32)(IPTR)proxy_GetBitMapAttr);
+    __AROS_SETVECADDRV0(abiv0GfxBase, 150, (APTR32)(IPTR)proxy_GetRGB32);
+    __AROS_SETVECADDRV0(abiv0GfxBase, 140, (APTR32)(IPTR)proxy_ObtainBestPenA);
+    __AROS_SETVECADDRV0(abiv0GfxBase,  33, graphicsjmp[202 -  33]);  // InitRastPort
+    __AROS_SETVECADDRV0(abiv0GfxBase,  11, graphicsjmp[202 -  11]);  // SetFont
+}
