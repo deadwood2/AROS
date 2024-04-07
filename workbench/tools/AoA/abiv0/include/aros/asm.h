7acb0d9ba2b93cd109a2bbeaebb3078769eb983c
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2024-04-07 16:17:27 +0200

    Support passing more than 6 arguments to 64-bit implementation
    This reworks how arguments are fetched to use r12 which is set in
    ALIGN_STACK64. This way the fetch offsets remain stable even when
    64-bit stack is being pushed onto (for arguments 7 and above)
    
    RESTORE_STACK64 resets rsp thus "freeing" arguments

--- workbench/tools/AoA/abiv0/include/aros/asm.h
+++ workbench/tools/AoA/abiv0/include/aros/asm.h
@@ -1,6 +1,8 @@
 #ifndef ABIV0_AROS_ASM_H
 #define ABIV0_AROS_ASM_H
 
+/* To be used in 32-bit code */
+
 #define ENTER64                 \
     "   subl $8, %%esp\n"       \
     "   movl $0x33, 4(%%esp)\n" \
@@ -9,6 +11,17 @@
     "   lret\n"                 \
     "   .code64\n"              \
     "1:\n"
+
+#define STORE_ESI_EDI           \
+    "   push %%edi\n"           \
+    "   push %%esi\n"           \
+
+#define RESTORE_ESI_EDI         \
+    "   pop %%esi\n"            \
+    "   pop %%edi\n"            \
+
+
+/* To be used in 64-bit code */
 
 #define ENTER32                 \
     "   subq $8, %%rsp\n"       \
@@ -23,35 +36,6 @@
     "   push $0x2b\n"           \
     "   pop %%es\n"
 
-
-#define STORE_ESI_EDI           \
-    "   push %%edi\n"           \
-    "   push %%esi\n"           \
-
-#define RESTORE_ESI_EDI         \
-    "   pop %%esi\n"            \
-    "   pop %%edi\n"            \
-
-/* +8 because of stored esi/edi */
-
-#define COPY_ARG_1              \
-    "   movl  4+8(%%rsp), %%edi\n"
-
-#define COPY_ARG_2              \
-    "   movl  8+8(%%rsp), %%esi\n"
-
-#define COPY_ARG_3              \
-    "   movl 12+8(%%rsp), %%edx\n"
-
-#define COPY_ARG_4              \
-    "   movl 16+8(%%rsp), %%ecx\n"
-
-#define COPY_ARG_5              \
-    "   movl 20+8(%%rsp), %%r8d\n"
-
-#define COPY_ARG_6              \
-    "   movl 24+8(%%rsp), %%r9d\n"
-
 #define ALIGN_STACK64           \
     "   push %%r12\n"           \
     "   movq %%rsp, %%r12\n"    \
@@ -60,5 +44,50 @@
 #define RESTORE_STACK64         \
     "   movq %%r12, %%rsp\n"    \
     "   popq %%r12\n"           \
+
+/* +8 because of stored esi/edi */
+/* +8 because of stored r12     */
+
+#define SET_ARG64__1_FROM32     \
+    "   movl  4+8+8(%%r12), %%edi\n"
+
+#define SET_ARG64__2_FROM32     \
+    "   movl  8+8+8(%%r12), %%esi\n"
+
+#define SET_ARG64__3_FROM32     \
+    "   movl 12+8+8(%%r12), %%edx\n"
+
+#define SET_ARG64__4_FROM32     \
+    "   movl 16+8+8(%%r12), %%ecx\n"
+
+#define SET_ARG64__5_FROM32     \
+    "   movl 20+8+8(%%r12), %%r8d\n"
+
+#define SET_ARG64__6_FROM32     \
+    "   movl 24+8+8(%%r12), %%r9d\n"
+
+#define SET_ARG64__7_FROM32     \
+    "   movl 28+8+8(%%r12), %%eax\n" \
+    "   push %%rax\n"
+
+#define SET_ARG64__8_FROM32     \
+    "   movl 32+8+8(%%r12), %%eax\n" \
+    "   push %%rax\n"
+
+#define SET_ARG64__9_FROM32     \
+    "   movl 36+8+8(%%r12), %%eax\n" \
+    "   push %%rax\n"
+
+#define SET_ARG64_10_FROM32     \
+    "   movl 40+8+8(%%r12), %%eax\n" \
+    "   push %%rax\n"
+
+#define SET_ARG64_11_FROM32     \
+    "   movl 44+8+8(%%r12), %%eax\n" \
+    "   push %%rax\n"
+
+#define SET_ARG64_12_FROM32     \
+    "   movl 48+8+8(%%r12), %%eax\n" \
+    "   push %%rax\n"
 
 #endif
--- workbench/tools/AoA/abiv0/include/aros/proxy.h
+++ workbench/tools/AoA/abiv0/include/aros/proxy.h
@@ -23,8 +23,8 @@ void dummy_##fname()            \
     EXTER_PROXY(fname)          \
     STORE_ESI_EDI               \
     ENTER64                     \
-    COPY_ARG_1                  \
     ALIGN_STACK64               \
+    SET_ARG64__1_FROM32         \
     CALL_IMPL64(fname)          \
     RESTORE_STACK64             \
     ENTER32                     \
@@ -39,9 +39,9 @@ void dummy_##fname()            \
     EXTER_PROXY(fname)          \
     STORE_ESI_EDI               \
     ENTER64                     \
-    COPY_ARG_1                  \
-    COPY_ARG_2                  \
     ALIGN_STACK64               \
+    SET_ARG64__2_FROM32         \
+    SET_ARG64__1_FROM32         \
     CALL_IMPL64(fname)          \
     RESTORE_STACK64             \
     ENTER32                     \
@@ -56,10 +56,10 @@ void dummy_##fname()            \
     EXTER_PROXY(fname)          \
     STORE_ESI_EDI               \
     ENTER64                     \
-    COPY_ARG_1                  \
-    COPY_ARG_2                  \
-    COPY_ARG_3                  \
     ALIGN_STACK64               \
+    SET_ARG64__3_FROM32         \
+    SET_ARG64__2_FROM32         \
+    SET_ARG64__1_FROM32         \
     CALL_IMPL64(fname)          \
     RESTORE_STACK64             \
     ENTER32                     \
@@ -74,11 +74,11 @@ void dummy_##fname()            \
     EXTER_PROXY(fname)          \
     STORE_ESI_EDI               \
     ENTER64                     \
-    COPY_ARG_1                  \
-    COPY_ARG_2                  \
-    COPY_ARG_3                  \
-    COPY_ARG_4                  \
     ALIGN_STACK64               \
+    SET_ARG64__4_FROM32         \
+    SET_ARG64__3_FROM32         \
+    SET_ARG64__2_FROM32         \
+    SET_ARG64__1_FROM32         \
     CALL_IMPL64(fname)          \
     RESTORE_STACK64             \
     ENTER32                     \
@@ -93,12 +93,12 @@ void dummy_##fname()            \
     EXTER_PROXY(fname)          \
     STORE_ESI_EDI               \
     ENTER64                     \
-    COPY_ARG_1                  \
-    COPY_ARG_2                  \
-    COPY_ARG_3                  \
-    COPY_ARG_4                  \
-    COPY_ARG_5                  \
     ALIGN_STACK64               \
+    SET_ARG64__5_FROM32         \
+    SET_ARG64__4_FROM32         \
+    SET_ARG64__3_FROM32         \
+    SET_ARG64__2_FROM32         \
+    SET_ARG64__1_FROM32         \
     CALL_IMPL64(fname)          \
     RESTORE_STACK64             \
     ENTER32                     \
@@ -113,13 +113,42 @@ void dummy_##fname()            \
     EXTER_PROXY(fname)          \
     STORE_ESI_EDI               \
     ENTER64                     \
-    COPY_ARG_1                  \
-    COPY_ARG_2                  \
-    COPY_ARG_3                  \
-    COPY_ARG_4                  \
-    COPY_ARG_5                  \
-    COPY_ARG_6                  \
     ALIGN_STACK64               \
+    SET_ARG64__6_FROM32         \
+    SET_ARG64__5_FROM32         \
+    SET_ARG64__4_FROM32         \
+    SET_ARG64__3_FROM32         \
+    SET_ARG64__2_FROM32         \
+    SET_ARG64__1_FROM32         \
+    CALL_IMPL64(fname)          \
+    RESTORE_STACK64             \
+    ENTER32                     \
+    RESTORE_ESI_EDI             \
+    LEAVE_PROXY                 \
+}
+
+/* With arguments 7 and above, be sure to maintain 16-byte aligment of stack by always copying
+   even number of arguments or pushing a dummy argument on stack */
+#define MAKE_PROXY_ARG_12(fname)\
+void proxy_##fname();           \
+void dummy_##fname()            \
+{                               \
+    EXTER_PROXY(fname)          \
+    STORE_ESI_EDI               \
+    ENTER64                     \
+    ALIGN_STACK64               \
+    SET_ARG64_12_FROM32         \
+    SET_ARG64_11_FROM32         \
+    SET_ARG64_10_FROM32         \
+    SET_ARG64__9_FROM32         \
+    SET_ARG64__8_FROM32         \
+    SET_ARG64__7_FROM32         \
+    SET_ARG64__6_FROM32         \
+    SET_ARG64__5_FROM32         \
+    SET_ARG64__4_FROM32         \
+    SET_ARG64__3_FROM32         \
+    SET_ARG64__2_FROM32         \
+    SET_ARG64__1_FROM32         \
     CALL_IMPL64(fname)          \
     RESTORE_STACK64             \
     ENTER32                     \
