7f0fb540fa69ab61db7954e6a0f8f860d278ec4c
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2024-05-02 15:08:26 +0200

    Adjust to change of location of stdc and posixc

--- compiler/crt/crt_wrapper.c
+++ compiler/crt/crt_wrapper.c
@@ -1,27 +1,27 @@
 #define remove(a) __stdc_remove(a)
-#include "../stdc/remove.c"
+#include "./stdc/remove.c"
 #undef remove
-#include "../posixc/remove.c"
-#include "../posixc/rmdir.c"
+#include "./posixc/remove.c"
+#include "./posixc/rmdir.c"
 
 #define rename(a, b) __stdc_rename(a, b)
-#include "../stdc/rename.c"
+#include "./stdc/rename.c"
 #undef rename
-#include "../posixc/rename.c"
+#include "./posixc/rename.c"
 
 /* Needs to be included here, as posixc/strerror is already included in mmakefile.src and
    these two files overalap by name */
-#include "../stdc/strerror.c"
+#include "./stdc/strerror.c"
 
 #define __stdc_geterrnoptr(a) ___geterrnoptr(a)
-#include "../stdc/__stdc_geterrnoptr.c"
+#include "./stdc/__stdc_geterrnoptr.c"
 
 #define __stdc_getsigngamptr(a) ___getsigngamptr(a)
-#include "../stdc/__stdc_getsigngamptr.c"
+#include "./stdc/__stdc_getsigngamptr.c"
 
 #define __stdc_mb_cur_max(a) ___mb_cur_max(a)
-#include "../stdc/__stdc_mb_cur_max.c"
+#include "./stdc/__stdc_mb_cur_max.c"
 
 #define __stdc_gmtoffset(a) ___gmtoffset(a)
-#include "../posixc/gettimeofday.c"
-#include "../posixc/clock_gettime.c"
+#include "./posixc/gettimeofday.c"
+#include "./posixc/clock_gettime.c"
--- compiler/crt/stdlib_wrapper.c
+++ compiler/crt/stdlib_wrapper.c
@@ -9,23 +9,23 @@
 #define strtoll(a, b, c)    __no_errno_strtoll(a, b, c)
 #define strtoull(a, b, c)   __no_errno_strtoull(a, b, c)
 
-#include "../stdc/strtoull.c"
-#include "../stdc/strtoll.c"
+#include "./stdc/strtoull.c"
+#include "./stdc/strtoll.c"
 
-#include "../stdc/atoll.c"
+#include "./stdc/atoll.c"
 
 #define strtol(a, b, c)     __no_errno_strtol(a, b, c)
 #define strtoul(a, b, c)    __no_errno_strtoul(a, b, c)
 
-#include "../stdc/strtoul.c"
-#include "../stdc/strtol.c"
+#include "./stdc/strtoul.c"
+#include "./stdc/strtol.c"
 
-#include "../stdc/atoi.c"
-#include "../stdc/atol.c"
-#include "../stdc/stcd_l.c"
-#include "../stdc/stch_l.c"
-#include "../stdc/stco_l.c"
+#include "./stdc/atoi.c"
+#include "./stdc/atol.c"
+#include "./stdc/stcd_l.c"
+#include "./stdc/stch_l.c"
+#include "./stdc/stco_l.c"
 
 #define __stdc_gmtoffset(a) ___gmtoffset(a)
-#include "../stdc/localtime_r.c"
+#include "./stdc/localtime_r.c"
 
