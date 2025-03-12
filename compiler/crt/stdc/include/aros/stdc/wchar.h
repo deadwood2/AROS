33d8a7708dea108081e01f943accd187b6dc3c1a
Deremon <65860936+Deremon@users.noreply.github.com>
2025-03-12 22:04:58 +0100

    Updated copyright notice in wchar.h

--- compiler/crt/stdc/include/aros/stdc/wchar.h
+++ compiler/crt/stdc/include/aros/stdc/wchar.h
@@ -1,7 +1,7 @@
 #ifndef _STDC_WCHAR_H_
 #define _STDC_WCHAR_H_
 /*
-    Copyright � 1995-2011, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2025, The AROS Development Team. All rights reserved.
     $Id$
 
     Standard C Library: Extended multibyte and wide character handling.
