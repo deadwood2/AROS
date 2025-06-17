abf3af3b5585a6922650f71a3fd9624cb2e14609
Kalamatee <kalamatee@gmail.com>
2025-06-17 19:21:03 +0100

    w.i.p update. provide inline iswprint & mbsinit to satisfy a2ps for now. flesh out locale use a little more. only enable experimental support if __WCHAR_MAX__ > 256

--- compiler/crt/stdc/include/aros/types/mbstate_t.h
+++ compiler/crt/stdc/include/aros/types/mbstate_t.h
@@ -6,6 +6,14 @@
     $Id$
 */
 
-typedef int mbstate_t;
+/*
+ * mbstate_t: Conversion state object for multibyte to wide character conversions.
+ * Internal fields used by the runtime to track conversion state.
+ */
+typedef struct {
+    int           __state;   /* internal use */
+    unsigned int  __count;
+    wchar_t       __value;
+} mbstate_t;
 
 #endif /* _AROS_TYPES_MBSTATE_T_H */
