89e7fe8103fe1ed20db18d9c181ad6fcb6476851
deadwood <deadwood@wp.pl>
2025-10-30 12:06:33 +0100

    Update version

--- workbench/tools/AoA/AoA.c
+++ workbench/tools/AoA/AoA.c
@@ -518,7 +518,7 @@ struct Device *TimerBase;
 
 STRPTR program_name = NULL;
 
-const TEXT version_string[] = "$VER: EmuV0 0.23 (29.10.2025)";
+const TEXT version_string[] = "$VER: EmuV0 0.24 (30.10.2025)";
 
 #define DEVMODE 0
 
