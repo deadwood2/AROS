5b353efd52eeb8f3fada219acd711191dafbf477
deadwood <deadwood@wp.pl>
2025-11-15 18:41:41 +0100

    Remove reset handler when freeing driver data
    Otherwise the node becomes invalid but is still kept in the handlers list
    and next time something accesses this list, it stumbles on random memory.
    
    Implementation copied from VIA-AC97 driver.

--- workbench/devs/AHI/Drivers/HDAudio/DriverData.h
+++ workbench/devs/AHI/Drivers/HDAudio/DriverData.h
@@ -182,7 +182,13 @@ struct HDAudioChip
 
     /** The recording software interrupt */
     struct Interrupt    record_interrupt;
-    
+
+    /** The reset handler */
+    struct Interrupt    reset_handler;
+
+    /** TRUE if the reset handler has been added to the system */
+    BOOL                reset_handler_added;
+
     /*** Card structures **************************************************/
     HDAADDR(playback_buffer1);
     HDAADDR(playback_buffer2);
