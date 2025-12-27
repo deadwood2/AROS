bb814a95b56685959fe578adc01a7a4e4bc75f03
Kalamatee <kalamatee@gmail.com>
2025-12-27 12:40:16 +0000

    quieten output by default.

--- rom/devs/ahci/ahci_aros.h
+++ rom/devs/ahci/ahci_aros.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2023, The AROS Development Team.  All rights reserved.
+ * Copyright (C) 2012-2025, The AROS Development Team.  All rights reserved.
  * Author: Jason S. McMullan <jason.mcmullan@gmail.com>
  *
  * Licensed under the AROS PUBLIC LICENSE (APL) Version 1.1
