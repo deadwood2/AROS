0ac8332c7a80f8e793faadbd5bfecf11d4448116
Neil Cafferkey <ncafferkey@users.noreply.github.com>
2025-12-28 08:52:06 +0000

    Backport: WirelessManager: fix for crash on exit

--- workbench/network/WirelessManager/wpa_supplicant/main_amiga.c
+++ workbench/network/WirelessManager/wpa_supplicant/main_amiga.c
@@ -1,7 +1,7 @@
 /*
  * WPA Supplicant / main() function for Amiga-like OSes
  * Copyright (c) 2003-2007, Jouni Malinen <j@w1.fi>
- * Copyright (c) 2010-2015, Neil Cafferkey
+ * Copyright (c) 2010-2025, Neil Cafferkey
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -45,7 +45,7 @@ typedef LONG PINT;
 #endif
 
 static const TEXT template[] = "DEVICE/A,UNIT/K/N,CONFIG/K,VERBOSE/S,NOGUI/S";
-const TEXT version_string[] = "$VER: WirelessManager 1.6 (8.3.2015)";
+const TEXT version_string[] = "$VER: WirelessManager 1.8 (29.11.2025)";
 static const TEXT config_file_name[] = "ENV:Sys/Wireless.prefs";
 
 
@@ -139,7 +139,6 @@ int main(int argc, char *argv[])
 out:
 	os_free(ifaces);
 
-//	os_program_deinit();
 #ifdef MUI_GUI
 	stop_gui();
 #endif
@@ -152,6 +151,8 @@ out:
 
 	if (error != 0)
 		exitcode = RETURN_FAIL;
+
+	os_program_deinit();
 
 	return exitcode;
 }
@@ -171,6 +172,7 @@ void stop_gui(void)
 {
 	if (gui_proc != NULL)
 	{
+		SetSignal(0, SIGF_SINGLE);
 		Signal((struct Task *) gui_proc, SIGBREAKF_CTRL_C);
 		Wait(SIGF_SINGLE);
 	}
