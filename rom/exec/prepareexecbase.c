40b92d63bebc6c331d5a1983bffebd2adc63fc2a
Kalamatee <kalamatee@gmail.com>
2025-12-31 09:54:22 +0000

    use %04x instead of %h.

--- rom/exec/prepareexecbase.c
+++ rom/exec/prepareexecbase.c
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 1995-2017, The AROS Development Team. All rights reserved.
+    Copyright (C) 1995-2025, The AROS Development Team. All rights reserved.
 
     Desc: Sets up the ExecBase a bit. (Mostly clearing).
 */
