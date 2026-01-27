94a4258fcf1140ff30562121535e4f63c5cb669b
Ivan Gualandri <dreamos82@yahoo.it>
2026-01-27 00:27:23 +0000

    Append full filename to panel status bar

--- workbench/system/Wanderer/panel_statusbar.c
+++ workbench/system/Wanderer/panel_statusbar.c
@@ -63,15 +63,6 @@ static struct List                      panelStatusBar__StatusBars;
 static struct NotifyRequest             panelStatusBar__PrefsNotifyRequest;
 static Object                           *panelStatusBar__PrefsNotificationObject;
 
-struct panel_StatusBar_DATA
-{
-    struct Node                         iwp_Node;
-    IPTR                                iwp_Flags;
-    Object                              *iwp_StatusBar_StatusBarObj;
-    Object                              *iwp_StatusBar_StatusTextObj;
-    struct Hook                         iwp_StatusBar_updateHook;
-};
-
 /// From panel_toolbar
 STRPTR ExpandEnvName(CONST_STRPTR env_path);
 ///
