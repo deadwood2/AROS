761e62e7205d18840e1c45a0f96bfb3233ef7a61
Nicolas Ramz <nicolas.ramz@gmail.com>
2026-07-24 12:20:50 +0200

    graphics: compose and accelerate area fills

--- rom/graphics/graphics_intern.h
+++ rom/graphics/graphics_intern.h
@@ -322,6 +322,12 @@ BOOL areafillpolygon(struct RastPort  * rp,
                      ULONG              bytesperrow,
                      struct GfxBase   * GfxBase);
 
+void areaoutlinepolygonmask(struct RastPort   *rp,
+                            struct Rectangle *bounds,
+                            UWORD              first_idx,
+                            UWORD              last_idx,
+                            ULONG              bytesperrow);
+
 void areafillellipse(struct RastPort   * rp,
 		     struct Rectangle  * bounds,
                      UWORD             * CurVctr,
