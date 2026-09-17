96eb47ff72733fac50c3318d72b699be6085f6ce
deadwood <deadwood@wp.pl>
2026-09-17 18:11:30 +0200

    [NP] Change include for alloca.h

--- compiler/include/clib/alib_protos.h
+++ compiler/include/clib/alib_protos.h
@@ -248,7 +248,7 @@ void GetDataStreamFromFormat(CONST_STRPTR format, va_list args,
 
 #ifdef AROS_SLOWSTACKFORMAT
 #define _GNU_SOURCE
-#include <aros/posixc/alloca.h>
+#include <alloca.h>
 
 #   define AROS_SLOWSTACKFORMAT_PRE_USING(last, format) {                                       \
     va_list _args;                      	                                                    \
