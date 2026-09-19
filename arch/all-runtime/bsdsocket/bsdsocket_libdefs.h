8d6b6e8d3a25eb8bd1c3efc1b7a0158d567e8af3
deadwood <deadwood@wp.pl>
2026-09-19 21:05:07 +0200

    Update version

--- arch/all-runtime/bsdsocket/bsdsocket_libdefs.h
+++ arch/all-runtime/bsdsocket/bsdsocket_libdefs.h
@@ -8,13 +8,13 @@
 #define LIBBASETYPE      struct SocketBase
 #define LIBBASETYPEPTR   struct SocketBase *
 #define MOD_NAME_STRING  "bsdsocket.library"
-#define MOD_DATE_STRING  "4.1.2024"
-#define MOD_VERS_STRING  "4.60"
+#define MOD_DATE_STRING  "19.9.2026"
+#define MOD_VERS_STRING  "4.65"
 #define VERSION_NUMBER   4
 #define MAJOR_VERSION    4
-#define REVISION_NUMBER  60
-#define MINOR_VERSION    60
-#define VERSION_STRING   "$VER: bsdsocket.library 4.60 (4.1.2024)\r\n"
+#define REVISION_NUMBER  65
+#define MINOR_VERSION    65
+#define VERSION_STRING   "$VER: bsdsocket.library 4.65 (19.9.2026)\r\n"
 #define COPYRIGHT_STRING ""
 #define LIBEND           GM_UNIQUENAME(End)
 #define LIBFUNCTABLE     GM_UNIQUENAME(FuncTable)
